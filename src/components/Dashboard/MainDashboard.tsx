2b4acfe78f2e344b4b732485b969e7f47ee642f0
Mohammad Al Amin <alamin.cse.99@gmail.com>
2025-12-23 17:22:26 +0600

    feat(dashboard): add dashboard components and layout
    - Create DashboardCard component for displaying metrics
    - Add Grow icon component for growth indicators
    - Implement MainDashboard component with metric cards
    - Update Dashboard page layout to include MainDashboard

---
+++ src/components/Dashboard/MainDashboard.tsx
@@ -0,0 +1,63 @@
+import { CloudSun, MailCheck, MapPin, Users } from "lucide-react";
+import DashboardCard from "./DashboardCard";
+
+export default function MainDashboard() {
+  const cards = [
+    {
+      title: "Total Location",
+      number: "6",
+      icon: <MapPin />,
+      paragraph: "from last months",
+      growth: "12%",
+      showGrowIcon: true,
+    },
+    {
+      title: "New Submissions",
+      number: "4",
+      icon: <MailCheck />,
+      growth: "Orders today",
+    },
+    {
+      title: "Active Users",
+      number: "1,247",
+      icon: <Users />,
+      paragraph: "from last months",
+      growth: "8%",
+      showGrowIcon: true,
+    },
+    {
+      title: "Weather API",
+      number: "Operational",
+      icon: <CloudSun />,
+      paragraph: "99.9% Uptime",
+    },
+  ];
+
+  return (
+    <div className="pl-8 pt-11 text-white">
+      <div>
+        <h1 className="text-[32px] font-inter font-semibold leading-[150%]">
+          Dashboard
+        </h1>
+        <p className="text-[16px] font-inter font-normal leading-[150%] text-[#F5F5F5A1] opacity-63 mt-2">
+          Welcome back, admin. Here's what's happening today.
+        </p>
+      </div>
+
+      {/* cards */}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 mt-8 pr-12">
+        {cards.map((item, index) => (
+          <DashboardCard
+            key={index}
+            title={item.title}
+            number={item.number}
+            icon={item.icon}
+            paragraph={item.paragraph}
+            growth={item.growth}
+            showGrowIcon={item.showGrowIcon}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,19 @@
+import MainDashboard from "../components/Dashboard/MainDashboard";
 import SIdebar from "../components/sidebar/SIdebar";
 import Topbar from "../components/topbar/Topbar";
 
 export default function Dashboard() {
   return (
     <>
-   <div className="flex">
-     <div className="w-[250px] border-r border-[rgba(59,175,122,0.2)]">
-        <SIdebar/>
-     </div>
-   <div className="w-[calc(100%-250px)]">
-      <Topbar/>
-   </div>
-   </div>
+      <div className="flex">
+        <div className="w-[250px] border-r border-[rgba(59,175,122,0.2)]">
+          <SIdebar />
+        </div>
+        <div className="w-[calc(100%-250px)]">
+          <Topbar />
+          <MainDashboard />
+        </div>
+      </div>
     </>
-  )
+  );
 }
