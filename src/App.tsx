c117281a4b89defa7e3209f3eb2dce78a4af1ed7
Mohammad Al Amin <alamin.cse.99@gmail.com>
2026-01-08 10:16:42 +0600

    running subscription

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Hikes from "./pages/Hikes";
 import Campgrounds from "./pages/Campgrounds";
 import Freedom from "./pages/Freedom";
 import Submission from "./pages/Submission";
+import Subscription from "./pages/Subscription";
 
 export default function App() {
   return (
@@ -16,7 +17,7 @@ export default function App() {
         <Route path="/location/campgrounds" element={<Campgrounds />} />
         <Route path="/location/freedom-camp" element={<Freedom />} />
         <Route path="/submission" element={<Submission />} />
-        <Route path="/system/subscription" element={<Dashboard />} />
+        <Route path="/system/subscription" element={<Subscription />} />
         <Route path="/system/notification" element={<Dashboard />} />
         <Route path="/system/activity-log" element={<Dashboard />} />
         <Route path="/advance/analytics" element={<Dashboard />} />
